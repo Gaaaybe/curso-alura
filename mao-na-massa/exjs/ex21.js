2d3c1fae6b41e47ae740664f9843445830d6b5e9
Gaaaybe <gabenesus@gmail.com>
2024-03-13 20:06:41 -0300

    Adiciona e remove elementos em um array de frutas

---
+++ mao-na-massa/exjs/ex21.js
@@ -0,0 +1,9 @@
+const frutas = ['Maçã', 'Banana', 'Laranja', 'Limão', 'Abacaxi'];
+
+console.log(frutas); // [ 'Maçã', 'Banana', 'Laranja', 'Limão', 'Abacaxi' ]
+
+// Remove 2 elementos a partir da posição 1 e adiciona 'kiwi' e 'pessêgo'
+
+frutas.splice(1, 2, 'kiwi', 'pessêgo');
+
+console.log(frutas); // [ 'Maçã', 'kiwi', 'pessêgo', 'Limão', 'Abacaxi' ]
