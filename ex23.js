428f96aa6f682719c5de4d5590bd6cf9c3563d69
Gabriel Menezes <gabenesus@gmail.com>
2024-03-14 09:16:52 -0300

    Adiciona função verificaNumero ao código

--- ex23.js
+++ ex23.js
@@ -1,4 +1,4 @@
-function executaOperacaoEmArray(array, callback){
+function executaOperacaoEmArray(array, callback) {
     array.forEach((element) => {
         console.log(callback(element))
     });
@@ -7,5 +7,15 @@ function executaOperacaoEmArray(array, callback){
 const array = [19, 20, 21, 22, 23, 24, 25, 26, 27, 28];
 
 executaOperacaoEmArray(array, (element) => {
-    return element *3;
+    return element * 3;
 }); // [ 57, 60, 63, 66, 69, 72, 75, 78, 81, 84 ]
+
+function verificaNumero(num, array) {
+    if (array.includes(num)) {
+        return array.indexOf(num);
+    } else {
+        return -1;
+    }
+}
+
+console.log(`O indice do numero buscado é ${verificaNumero(18, array)}`); // -1
