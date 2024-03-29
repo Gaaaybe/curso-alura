aa098566fbc5097d4abd4917edc3746c2e63f96d
Gaaaybe <gabenesus@gmail.com>
2024-03-29 16:21:16 -0300

    Atualizações no código para extrair e exibir links de um arquivo

---
+++ projetos/lib-testa-links/src/index.js
@@ -0,0 +1,38 @@
+import fs from 'fs';
+import chalk from 'chalk';
+
+
+function trataErro(erro) {
+  throw new Error(chalk.red(erro.code));
+}
+
+function extraiLinks(texto) {
+    const regex = /\[([^[\]]*?)\]\((https?:\/\/[^\s?#.].[^\s]*)\)/gm;
+    const capturas = [...texto.matchAll(regex)];
+    const resultados = capturas.map(captura => ({[captura[1]] : captura[2]}));
+    return resultados;
+}
+
+// async/await
+
+async function pegaArquivo(caminhoDoArquivo) {
+  try {
+    const encoding = 'utf-8';
+    const texto = await fs.promises.readFile(caminhoDoArquivo, encoding)
+    console.log(extraiLinks(texto));
+  } catch (erro) {
+    trataErro(erro)
+  }
+}
+
+// promises then/catch
+
+// function pegaArquivo(caminhoDoArquivo) {
+//   const encoding = 'utf-8';
+//   fs.promises
+//     .readFile(caminhoDoArquivo, encoding)
+//     .then((texto) => console.log(chalk.green(texto)))
+//     .catch(trataErro)
+// }
+
+pegaArquivo('./arquivos/texto.md');
