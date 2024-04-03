74a4b5bc279e7f55df4bc87312d9efddc12cc411
Gaaaybe <gabenesus@gmail.com>
2024-04-03 17:50:26 -0300

    Adiciona interatividade aos botões e atualiza o título da página

--- projetos/Fokus-projeto/script.js
+++ projetos/Fokus-projeto/script.js
@@ -3,20 +3,50 @@ const btFoco = document.querySelector('.app__card-button--foco');
 const btCurto = document.querySelector('.app__card-button--curto');
 const btLongo = document.querySelector('.app__card-button--longo');
 const banner = document.querySelector('.app__image');
+const title = document.querySelector('.app__title');
+const botoes = document.querySelectorAll('.app__card-button');
 
 btFoco.addEventListener('click', () => {
     alterarContexto('foco');
+    btFoco.classList.add('active');
 });
 
 btCurto.addEventListener('click', () => {
     alterarContexto('descanso-curto');
+    btCurto.classList.add('active');
 });
 
 btLongo.addEventListener('click', () => {
     alterarContexto('descanso-longo');
+    btLongo.classList.add('active');
 });
 
 function alterarContexto(contexto) {
+    botoes.forEach((contexto) => {
+        contexto.classList.remove('active');
+    });
     html.setAttribute('data-contexto', contexto);
     banner.setAttribute('src', `./imagens/${contexto}.png`);
+    
+    switch (contexto) {
+        case 'foco':
+            title.innerHTML = `
+            Otimize sua produtividade,<br>
+                <strong class="app__title-strong">mergulhe no que importa.</strong>
+            `;
+            break;
+        case 'descanso-curto':
+            title.innerHTML = `
+            Que tal dar uma respirada? <strong class="app__title-strong">Faça uma pausa curta!</strong>
+            `;
+            break;
+        case 'descanso-longo':
+            title.innerHTML = `
+            Hora de voltar à superfície,<br>
+                <strong class="app__title-strong">Faça uma pausa longa.</strong>
+            `;
+            break;    
+        default:
+            break;
+    }
 };
