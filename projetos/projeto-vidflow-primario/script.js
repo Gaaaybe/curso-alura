aa7ee447947811f01259c9b6aeb6eeeb8a9ef5b3
Gaaaybe <gabenesus@gmail.com>
2024-04-10 23:34:07 -0300

    Adiciona script para carregar vídeos na página principal do projeto VidFlow

---
+++ projetos/projeto-vidflow-primario/script.js
@@ -0,0 +1,13 @@
+const containerVideos = document.querySelector('.videos__container');
+const api = fetch('http://localhost:3000/videos')
+    .then( res => res.json())
+    .then((videos) => videos.forEach((video) => {
+        containerVideos.innerHTML += `
+        <li class="videos__item"> 
+            <iframe src="${video.url}" title="${video.titulo}" frameborder="0" allowfullscreen></iframe>
+            <div class="descricao-video">
+                <img class="img-canal" src="${video.imagem}" alt="Logo do Canal">
+            </div>
+        </li>
+        `;
+    }))
