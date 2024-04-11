cc7018b141cc9f824fbc2a1f3c4db7e047e4cf09
Gabriel Menezes <gabenesus@gmail.com>
2024-04-11 09:20:15 -0300

    Adiciona tratamento de erro ao carregar vídeos

--- projetos/projeto-vidflow-primario/script.js
+++ projetos/projeto-vidflow-primario/script.js
@@ -7,7 +7,12 @@ const api = fetch('http://localhost:3000/videos')
             <iframe src="${video.url}" title="${video.titulo}" frameborder="0" allowfullscreen></iframe>
             <div class="descricao-video">
                 <img class="img-canal" src="${video.imagem}" alt="Logo do Canal">
+                <h3 class="titulo-video">${video.titulo}</h3>
+                <p class="titulo-canal">${video.descricao}</p>
             </div>
         </li>
         `;
     }))
+    .catch((error) => {
+        containerVideos.innerHTML = `<p> Houve um erro ao carregar os videos ${error} </p>`;
+    });
