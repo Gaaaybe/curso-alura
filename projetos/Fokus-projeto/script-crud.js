b793a6ee1ecb7e0c73e2e92aed276e096c7ee61a
Gaaaybe <gabenesus@gmail.com>
2024-04-13 13:01:23 -0300

    Corrige funcionalidade de edição de tarefa no script de CRUD

--- projetos/Fokus-projeto/script-crud.js
+++ projetos/Fokus-projeto/script-crud.js
@@ -5,24 +5,38 @@ const ulTarefas = document.querySelector('.app__section-task-list')
 
 const tarefas = JSON.parse(localStorage.getItem('tarefas')) || [];
 
+function atualizarLocalStorage(){
+    localStorage.setItem('tarefas', JSON.stringify(tarefas));
+}
+
 function criarTarefa(tarefa){
     const li = document.createElement('li');
     li.classList.add('app__section-task-list-item');
 
     const svg = document.createElement('svg');
     svg.innerHTML = `    
-    <svg>
         <svg class="app__section-task-icon-status" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
             <circle cx="12" cy="12" r="12" fill="#FFF"></circle>
             <path d="M9 16.1719L19.5938 5.57812L21 6.98438L9 18.9844L3.42188 13.4062L4.82812 12L9 16.1719Z" fill="#01080E"></path>
         </svg>
-    </svg>
     `;
 
     const p = document.createElement('p');
     p.textContent = tarefa.descricao;
+    p.classList.add('app__section-task-list-item-description');
 
     const button = document.createElement('button');
+    button.classList.add('app_button-edit');
+
+    button.onclick = () => {
+        const novaDescricao = prompt('Qual o novo nome da tarefa?');
+        if (novaDescricao) {
+            p.textContent = novaDescricao;
+            tarefa.descricao = novaDescricao;
+            atualizarLocalStorage();
+        };
+    };
+
     const svgButton = document.createElement('img');
     svgButton.setAttribute('src', '/imagens/edit.png');
     button.append(svgButton);
@@ -44,7 +58,11 @@ formAdicionarTarefa.addEventListener('submit', (evento) => {
         descricao: textarea.value
     };
     tarefas.push(tarefa);
-    localStorage.setItem('tarefas', JSON.stringify(tarefas));
+    const elementoTarefa = criarTarefa(tarefa);
+    ulTarefas.append(elementoTarefa);
+    atualizarLocalStorage();
+    textarea.value = '';
+    formAdicionarTarefa.classList.add('hidden');
 });
 
 tarefas.forEach((tarefa) => {
