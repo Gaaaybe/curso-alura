075e4c470e48d1bec47e2a18a366d0f40fc6b5b2
Gaaaybe <gabenesus@gmail.com>
2024-04-13 14:18:18 -0300

    Adiciona evento de FocoTerminado ao finalizar o tempo no script.js

--- projetos/Fokus-projeto/script.js
+++ projetos/Fokus-projeto/script.js
@@ -80,6 +80,11 @@ const contagemRegressiva = () => {
     if(tempoDecorridoEmSegundos <= 0){
         //audioTempoFinalizado.play();
         alert('Tempo finalizado!');
+        const focoAtivo = html.getAttribute('data-contexto') === 'foco';
+        if (focoAtivo) {
+            const evento = new CustomEvent('FocoTerminado');
+            document.dispatchEvent(evento);
+        }
         zerar();
         return;
     };
