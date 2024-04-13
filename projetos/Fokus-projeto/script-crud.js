48e58e5514ec3b39ae97b9a7ac4ba7d07188bcd9
Gaaaybe <gabenesus@gmail.com>
2024-04-13 10:58:55 -0300

    Adiciona funcionalidade de criar tarefa no script de CRUD

--- projetos/Fokus-projeto/script-crud.js
+++ projetos/Fokus-projeto/script-crud.js
@@ -1,18 +1,45 @@
-const btnAdicionarTarefa = document.querySelector('.app__button--add-task')
-const formAdicionarTarefa = document.querySelector('.app__form-add-task')
-const textarea = document.querySelector('.app__form-textarea')
+const btnAdicionarTarefa = document.querySelector('.app__button--add-task');
+const formAdicionarTarefa = document.querySelector('.app__form-add-task');
+const textarea = document.querySelector('.app__form-textarea');
 
-const tarefas = []
+const tarefas = JSON.parse(localStorage.getItem('tarefas')) || [];
+
+function criarTarefa(tarefa){
+    const li = document.createElement('li');
+    li.classList.add('app__section-task-list-item');
+
+    const svg = document.createElement('svg');
+    svg.innerHTML = `    
+    <svg>
+        <svg class="app__section-task-icon-status" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <circle cx="12" cy="12" r="12" fill="#FFF"></circle>
+            <path d="M9 16.1719L19.5938 5.57812L21 6.98438L9 18.9844L3.42188 13.4062L4.82812 12L9 16.1719Z" fill="#01080E"></path>
+        </svg>
+    </svg>
+    `;
+
+    const p = document.createElement('p');
+    p.textContent = tarefa.descricao;
+
+    const button = document.createElement('button');
+    const svgButton = document.createElement('img');
+    svgButton.setAttribute('src', '/imagens/edit.png');
+    button.append(svgButton);
+
+    li.append(svg);
+    li.append(p);
+    li.append(button);
+};
 
 btnAdicionarTarefa.addEventListener('click', () => {
-    formAdicionarTarefa.classList.toggle('hidden')
-})
+    formAdicionarTarefa.classList.toggle('hidden');
+});
 
 formAdicionarTarefa.addEventListener('submit', (evento) => {
     evento.preventDefault();
     const tarefa = {
         descricao: textarea.value
-    }
-    tarefas.push(tarefa)
-    localStorage.setItem('tarefas', JSON.stringify(tarefas))
-})
+    };
+    tarefas.push(tarefa);
+    localStorage.setItem('tarefas', JSON.stringify(tarefas));
+});
