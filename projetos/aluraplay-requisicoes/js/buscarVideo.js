06e843bdf9b598b86be015220087860ad51648cb
Gaaaybe <gabenesus@gmail.com>
2024-04-22 14:13:30 -0300

    Adiciona função para exibir vídeos na página principal

--- projetos/aluraplay-requisicoes/js/buscarVideo.js
+++ projetos/aluraplay-requisicoes/js/buscarVideo.js
@@ -1,11 +1,16 @@
 import { conectaApi } from "./conectaApi.js";
+import { constroiCard } from "./mostrarVideos.js";
 
 async function buscarVideo(event){
     event.preventDefault();
     const dadosDePesquisa = document.querySelector('[data-pesquisa]').value;
     const busca = await conectaApi.buscaVideo(dadosDePesquisa);
 
-    console.log(busca);
+    const lista = document.querySelector('[data-lista]');
+
+    busca.forEach(element => {
+        lista.appendChild(constroiCard(element.titulo, element.descricao, element.url, element.imagem));
+    });
 }
 
 const botaoDePesquisa = document.querySelector('[data-botao-pesquisa]');
