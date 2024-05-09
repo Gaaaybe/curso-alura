f8edade3a78736aef071f671203400f4d243b263
Gabriel Menezes <gabenesus@gmail.com>
2024-05-09 10:41:19 -0300

    feat: Corrige mensagens de erro no formulário de cadastro

--- projetos/monibank/js/script.js
+++ projetos/monibank/js/script.js
@@ -1,4 +1,4 @@
-import validaCPF  from "./validaCPF.js";
+import validaCPF from "./validaCPF.js";
 import validaIdade from "./validaIdade.js";
 
 const camposDoFormulario = document.querySelectorAll('[required]');
@@ -8,7 +8,7 @@ camposDoFormulario.forEach((campo) => {
     campo.addEventListener("invalid", event => event.preventDefault());
 });
 
-const tiposDeErro = [
+const tiposErro = [
     "valueMissing",
     "typeMismatch",
     "patternMismatch",
@@ -16,11 +16,58 @@ const tiposDeErro = [
     "customError"
 ];
 
-function verificaCampo(campo) {
-    if(campo.name === "cpf" && campo.value.length >= 11) {
-        validaCPF(campo);
+const mensagens = {
+    nome: {
+        valueMissing: "O campo de nome não pode estar vazio.",
+        patternMismatch: "Por favor, preencha um nome válido.",
+        tooShort: "Por favor, preencha um nome válido."
+    },
+    email: {
+        valueMissing: "O campo de e-mail não pode estar vazio.",
+        typeMismatch: "Por favor, preencha um email válido.",
+        tooShort: "Por favor, preencha um e-mail válido."
+    },
+    rg: {
+        valueMissing: "O campo de RG não pode estar vazio.",
+        patternMismatch: "Por favor, preencha um RG válido.",
+        tooShort: "O campo de RG não tem caractéres suficientes."
+    },
+    cpf: {
+        valueMissing: 'O campo de CPF não pode estar vazio.',
+        patternMismatch: "Por favor, preencha um CPF válido.",
+        customError: "O CPF digitado não existe.",
+        tooShort: "O campo de CPF não tem caractéres suficientes."
+    },
+    aniversario: {
+        valueMissing: 'O campo de data de nascimento não pode estar vazio.',
+        customError: 'Você deve ser maior que 18 anos para se cadastrar.'
+    },
+    termos: {
+        valueMissing: 'Você deve aceitar nossos termos antes de continuar.',
     }
-    if(campo.name === "aniversario" && campo.value != "") {
+};
+
+function verificaCampo(campo) {
+    let mensagem = "";
+    if (campo.name === "cpf" && campo.value.length >= 11) {
+        validaCPF(campo);
+    };
+    if (campo.name === "aniversario" && campo.value != "") {
         validaIdade(campo);
+    };
+    tiposErro.forEach(erro => {
+        if (campo.validity[erro]) {
+            mensagem = mensagens[campo.name][erro];
+            console.log(mensagem);
+        }
+    });
+
+    const mensagemErro = campo.parentNode.querySelector('.mensagem-erro');
+    const validador = campo.checkValidity();
+
+    if (!validador) {
+        mensagemErro.textContent = mensagem;
+    } else{
+        mensagemErro.textContent = "";
     }
 };
