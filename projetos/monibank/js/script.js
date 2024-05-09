4f16c996a0cf9921431570a767689d80eb34c182
Gabriel Menezes <gabenesus@gmail.com>
2024-05-09 10:21:28 -0300

    feat: Adiciona validação de campos no formulário de cadastro

--- projetos/monibank/js/script.js
+++ projetos/monibank/js/script.js
@@ -4,8 +4,17 @@ import validaIdade from "./validaIdade.js";
 const camposDoFormulario = document.querySelectorAll('[required]');
 
 camposDoFormulario.forEach((campo) => {
-    campo.addEventListener("blur", () => verificaCampo(campo))
+    campo.addEventListener("blur", () => verificaCampo(campo));
+    campo.addEventListener("invalid", event => event.preventDefault());
 });
+
+const tiposDeErro = [
+    "valueMissing",
+    "typeMismatch",
+    "patternMismatch",
+    "tooShort",
+    "customError"
+];
 
 function verificaCampo(campo) {
     if(campo.name === "cpf" && campo.value.length >= 11) {
