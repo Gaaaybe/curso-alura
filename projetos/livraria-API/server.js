5c3be93918ebdd6338aab9c51302ed51f865044f
Gaaaybe <gabenesus@gmail.com>
2024-05-15 19:31:50 -0300

    Iniciando projeto API de uma livraria e criando primeiro server

---
+++ projetos/livraria-API/server.js
@@ -0,0 +1,16 @@
+import http from "http";
+
+const PORT = 3000;
+
+const rotas = {
+    "/": "Curso de Node.js"
+};
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end(rotas[req.url]);
+});
+
+server.listen(PORT, () => {
+    console.log("Servidor rodando na porta", PORT);
+});
