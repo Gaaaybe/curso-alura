febe7ff42f1133ade10c9b87078dbe2c3ab373f3
Gaaaybe <gabenesus@gmail.com>
2024-05-25 15:42:40 -0300

    feat: Atualiza modelo de Livro adicionando campos obrigatórios e removendo versão

--- projetos/livraria-API/src/models/Livro.js
+++ projetos/livraria-API/src/models/Livro.js
@@ -1,1 +1,13 @@
-import
+import mongoose from "mongoose";
+
+const LivroSchema = new mongoose.Schema({
+    id: { type: mongoose.Schema.Types.ObjectId },
+    titulo: { type: String, required: true },
+    editora: { type: String, required: true },
+    preco: { type: Number, required: true },
+    paginas: { type: Number, required: true },
+}, { versionKey: false });
+
+const livro = mongoose.model("Livro", LivroSchema);
+
+export default livro;
