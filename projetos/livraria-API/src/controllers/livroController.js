aef066337d180eb55c01dc5d52f5d8cae0cc358e
Gaaaybe <gabenesus@gmail.com>
2024-05-26 20:41:07 -0300

    feat: Atualiza rota de listagem de livros para buscar no banco de dados

--- projetos/livraria-API/src/controllers/livroController.js
+++ projetos/livraria-API/src/controllers/livroController.js
@@ -11,6 +11,16 @@ class LivroController {
         }
       };
 
+      static async listarLivroPorId (req, res) {
+        try {
+          const id = req.params.id;
+          const livroEncontrado = await livro.findById(id);
+          res.status(200).json(livroEncontrado);
+        } catch (erro) {
+          res.status(500).json({ message: `${erro.message} - falha na requisição` });
+        }
+      };
+
     static async cadastrarLivro(req, res) {
         try {
             const novoLivro = await livro.create(req.body);
@@ -20,6 +30,16 @@ class LivroController {
         } catch (erro) {
             res.status(500).json({ message: `${erro.message} - Falha ao cadastrar livro.` });
         };
+    };
+
+    static async atualizarLivro (req, res) {
+      try {
+        const id = req.params.id;
+        await livro.findByIdAndUpdate(id, req.body);
+        res.status(200).json({ message: "Atualizado com sucesso!" });
+      } catch (erro) {
+        res.status(500).json({ message: `${erro.message} - falha na requisição` });
+      }
     };
 };
 
