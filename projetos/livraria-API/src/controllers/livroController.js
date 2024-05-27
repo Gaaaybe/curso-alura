49c6d91a40f05fb0a5acff7f6bb93c7fef3c8a7c
Gaaaybe <gabenesus@gmail.com>
2024-05-26 21:02:01 -0300

    feat: Adiciona método de exclusão de livro na API da livraria

--- projetos/livraria-API/src/controllers/livroController.js
+++ projetos/livraria-API/src/controllers/livroController.js
@@ -41,6 +41,16 @@ class LivroController {
         res.status(500).json({ message: `${erro.message} - falha na requisição` });
       }
     };
+
+    static async excluirLivro (req, res) {
+      try {
+        const id = req.params.id;
+        await livro.findByIdAndDelete(id);
+        res.status(200).json({ message: "Excluido com sucesso!" });
+      } catch (erro) {
+        res.status(500).json({ message: `${erro.message} - falha na requisição` });
+      }
+    };
 };
 
 export default LivroController;
