6e1267bb84a3a83105476c56e34de383dadfe536
Gaaaybe <gabenesus@gmail.com>
2024-05-27 23:47:33 -0300

    feat: Atualiza cadastro de livro na API da livraria com informações completas do autor

--- projetos/livraria-API/src/controllers/livroController.js
+++ projetos/livraria-API/src/controllers/livroController.js
@@ -1,4 +1,5 @@
 import livro from '../models/Livro.js';
+import { autor } from '../models/Autor.js';
 
 class LivroController {
 
@@ -22,10 +23,13 @@ class LivroController {
       };
 
     static async cadastrarLivro(req, res) {
+        const novoLivro = req.body;
         try {
-            const novoLivro = await livro.create(req.body);
+            const autorEncontrado = await autor.findById(novoLivro.autor);
+            const livroCompleto = { ...novoLivro, autor: {...autorEncontrado._doc} };
+            const livroCriado = await livro.create(livroCompleto);
             res.status(201).json({
-                message: "Criado com sucesso!", livro: novoLivro
+                message: "Criado com sucesso!", livro: livroCriado
             });
         } catch (erro) {
             res.status(500).json({ message: `${erro.message} - Falha ao cadastrar livro.` });
--- projetos/livraria-API/src/models/Livro.js
+++ projetos/livraria-API/src/models/Livro.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import { AutorSchema } from "./Autor.js";
 
 const LivroSchema = new mongoose.Schema({
     id: { type: mongoose.Schema.Types.ObjectId },
@@ -6,6 +7,7 @@ const LivroSchema = new mongoose.Schema({
     editora: { type: String, required: true },
     preco: { type: Number, required: true },
     paginas: { type: Number, required: true },
+    autor: AutorSchema
 }, { versionKey: false });
 
 const livro = mongoose.model("livros", LivroSchema);
